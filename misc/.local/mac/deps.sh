f2d22f9a646c6edd1c396f4116340d76257421d1
chrsm <git@c.chrsm.org>
2025-11-17 19:34:00 -0800

    misc stuff

---
+++ misc/.local/mac/deps.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+set -euxo pipefail
+
+function install_redeyes() {
+	git clone https://github.com/tonylazarew/redeyes ~/.local/deps/redeyes
+	cd ~/.local/deps/redeyes && git reset --hard 4a99bd0894e0078f3e742cab2279c8e2350b3b1b
+	make && cp redeyes ~/.local/bin
+}

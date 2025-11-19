5a8c5f863e4065a0907882bec7f16c5478259411
chrsm <git@c.chrsm.org>
2025-11-18 21:06:44 -0800

    misc: ghostty+mac stuff

--- misc/.local/mac/setup.sh
+++ misc/.local/mac/setup.sh
@@ -6,19 +6,19 @@ defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-defaults write com.apple.LaunchServices LSQuarantine -bool false
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 defaults write NSGlobalDomain AppleFontSmoothing -int 1
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder AppleShowAllFiles TRUE
+defaults write com.apple.finder AppleShowAllFiles -boolean true
 
 defaults write com.apple.dock minimize-to-application -bool true
 defaults write com.apple.dock show-process-indicators -bool true
@@ -28,12 +28,12 @@ defaults write com.apple.dock autohide-time-modifier -float 0
 defaults write com.apple.dock showhidden -bool true
 defaults write com.apple.dock show-recents -bool false
 
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 # stupid key-hold menu
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 # shit's loud
 sudo nvram SystemAudioVolume=" "
-
-defaults write com.apple.finder AppleShowAllFiles -boolean true
 
 killall Finder
